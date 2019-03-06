dd30bb8a233af45786c9084a2fc71376910c4c95
Pattawan Kaewduangdee <pattawanna@gmail.com>
2019-03-06 12:24:33 +0100

    add function to add leading sign

--- util/transform-data.js
+++ util/transform-data.js
@@ -26,20 +26,33 @@ function calDrawdown(data) {
 }
 
 function calReturn(data) {
-  const first = data.shift();
-  const last =  data.pop();
+  const first = data[0];
+  const last =  data[data.length - 1];
   const percent = ((last.close - first.close) / first.close) * 100;
   
+
+  
   const returnValue = {
-    fromDate: first.date,
-    toDate: last.date,
+    startDate: first.date,
+    endDate: last.date,
     fromValue: first.close,
     toValue: last.close,
     rate: last.close - first.close,
-    percent: percent.toFixed(1)
+    percent: addLeadingSign(percent)
   };
   
   return returnValue;
+}
+
+function addLeadingSign(number) {
+  switch(Math.sign(number)) {
+    case 1: return `+${number.toFixed(1)}`;
+      break;
+    case -1: return `-${number.toFixed(1)}`;
+      break;
+    case 0:
+      return number.toFixed(1);
+  }
 }
 
 module.exports = {
